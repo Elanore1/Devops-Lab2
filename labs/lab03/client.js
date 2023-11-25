eb12e2925eb82adc999e2067bb1c0c8ec00431ac
Noé Pham <noe.pham@edu.ece.fr>
2023-11-25 16:13:47 +0100

    feat: vagrant and docker implemented but not ready yet

--- labs/lab03/client.js
+++ labs/lab03/client.js
@@ -4,23 +4,24 @@ const redis = require("redis");
 // If REDIS_HOST is not set, the default host is localhost
 // If REDIS_PORT is not set, the default port is 6379
 const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+  url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
 });
 
 redisClient.on("error", (err) => console.log("Error", err));
 
 (async () => {
-  await redisClient.connect();
-
   // Sets the key "octocat" to a value of "Mona the octocat"
   const setKeyReply = await redisClient.set("octocat", "Mona the Octocat");
   console.log("Reply: " + setKeyReply);
+
   // Sets a key to "species", field to "octocat", and "value" to "Cat and Octopus"
   const SetFieldOctocatReply = await redisClient.hSet("species", "octocat", "Cat and Octopus");
   console.log("Reply: " + SetFieldOctocatReply);
+
   // Sets a key to "species", field to "dinotocat", and "value" to "Dinosaur and Octopus"
   const SetFieldDinotocatReply = await redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus");
   console.log("Reply: " + SetFieldDinotocatReply);
+
   // Sets a key to "species", field to "robotocat", and "value" to "Cat and Robot"
   const SetFieldRobotocatReply = await redisClient.hSet("species", "robotocat", "Cat and Robot");
   console.log("Reply: " + SetFieldRobotocatReply);
@@ -30,11 +31,12 @@ redisClient.on("error", (err) => console.log("Error", err));
     const replies = await redisClient.hKeys("species");
     console.log(replies.length + " replies:");
     replies.forEach((reply, i) => {
-        console.log("    " + i + ": " + reply);
+      console.log("    " + i + ": " + reply);
     });
-    await redisClient.quit();
-  }
-  catch (err) {
-    // statements to handle any exceptions
+  } catch (err) {
+    console.error("Error during operation:", err);
+  } finally {
+    // Gracefully close the Redis connection
+    redisClient.quit();
   }
 })();
