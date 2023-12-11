b026bcacc2bc988a11bcb30a78ae3cbd9df069a1
Noé Pham <noe.pham@edu.ece.fr>
2023-12-11 11:33:09 +0100

    fix:docker-compse,dbclient,index.js

--- labs/lab03/src/routes/user.js
+++ labs/lab03/src/routes/user.js
@@ -1,53 +1,44 @@
-const express = require('express');
-const userController = require('../controllers/user');
+const express = require('express')
+const userController = require('../controllers/user')
 
-const userRouter = express.Router();
+const userRouter = express.Router()
 
 userRouter
   .post('/', (req, resp) => {
     userController.create(req.body, (err, res) => {
-      let respObj;
-      if (err) {
+      let respObj
+      if(err) {
         respObj = {
           status: "error",
           msg: err.message
-        };
-        return resp.status(400).json(respObj);
+        }
+        return resp.status(400).json(respObj)
       }
       respObj = {
         status: "success",
         msg: res
-      };
-      resp.status(201).json(respObj);
-    });
+      }
+      resp.status(201).json(respObj)
+    })
   })
-  .get('/:username', (req, resp) => {
-    const username = req.params.username;
-
-    userController.getUserByUsername(username, (err, user) => {
-      let respObj;
-      if (err) {
+  .get('/:username', (req, resp) => { // Express URL params - https://expressjs.com/en/guide/routing.html
+    // TODO Create get method API
+    const username = req.params.username
+    userController.get(username, (err, res) => {
+      let respObj
+      if(err) {
         respObj = {
           status: "error",
           msg: err.message
-        };
-        return resp.status(500).json(respObj); // Internal Server Error
+        }
+        return resp.status(400).json(respObj)
       }
-
-      if (!user) {
-        respObj = {
-          status: "error",
-          msg: "User not found"
-        };
-        return resp.status(404).json(respObj); // Not Found
-      }
-
       respObj = {
         status: "success",
-        user
-      };
-      resp.status(200).json(respObj);
-    });
-  });
-
-module.exports = userRouter;
+        msg: res
+      }
+      resp.status(200).json(respObj)
+    })
+  })
+  
+module.exports = userRouter
