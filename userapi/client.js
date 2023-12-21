97c23cb172b9e581a288580c3a15ba45a4918cfa
Noé Pham <noe.pham@edu.ece.fr>
2023-12-21 18:18:08 +0100

    fix:file placement correction and fix code

---
+++ userapi/client.js
@@ -0,0 +1,41 @@
+const redis = require("redis");
+
+// Creates a new Redis client
+// If REDIS_HOST is not set, the default host is localhost
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`
+});
+
+redisClient.on("error", (err) => console.log("Error", err));
+
+(async () => {
+  // Sets the key "octocat" to a value of "Mona the octocat"
+  const setKeyReply = await redisClient.set("octocat", "Mona the Octocat");
+  console.log("Reply: " + setKeyReply);
+
+  // Sets a key to "species", field to "octocat", and "value" to "Cat and Octopus"
+  const SetFieldOctocatReply = await redisClient.hSet("species", "octocat", "Cat and Octopus");
+  console.log("Reply: " + SetFieldOctocatReply);
+
+  // Sets a key to "species", field to "dinotocat", and "value" to "Dinosaur and Octopus"
+  const SetFieldDinotocatReply = await redisClient.hSet("species", "dinotocat", "Dinosaur and Octopus");
+  console.log("Reply: " + SetFieldDinotocatReply);
+
+  // Sets a key to "species", field to "robotocat", and "value" to "Cat and Robot"
+  const SetFieldRobotocatReply = await redisClient.hSet("species", "robotocat", "Cat and Robot");
+  console.log("Reply: " + SetFieldRobotocatReply);
+
+  try {
+    // Gets all fields in "species" key
+    const replies = await redisClient.hKeys("species");
+    console.log(replies.length + " replies:");
+    replies.forEach((reply, i) => {
+      console.log("    " + i + ": " + reply);
+    });
+  } catch (err) {
+    console.error("Error during operation:", err);
+  } finally {
+    // Gracefully close the Redis connection
+    redisClient.quit();
+  }
+})();
